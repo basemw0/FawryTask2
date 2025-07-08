2f7e6a85d88637b7056a5b574b990f5b3f234410
Basem Walid <basemwalid44@gmail.com>
2025-07-08 21:22:55 +0300

    Delete ShippingService.java

--- ShippingService.java
+++
@@ -1,6 +0,0 @@
-
-public class ShippingService {
-	 public static void sendItems(Shippable item, String address) {
-        System.out.println("Shipping "  + item.getTitle() + " to address: " + address);
-    }
-}
