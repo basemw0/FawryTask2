bde3c6ea44502f02b79112cffbc9fbeb08aa0e88
Basem Walid <basemwalid44@gmail.com>
2025-07-08 21:20:16 +0300

    Delete MailService.java

--- MailService.java
+++
@@ -1,5 +0,0 @@
-public class MailService {
-    public static void sendEbook(String email, String title, String fileType) {
-        System.out.println("Sending " + title + " (" + fileType + ") to " + email);
-    }
-}
